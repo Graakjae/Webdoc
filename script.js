f6d4ae225c68fa29e8ee0e61628515bcba9ff533
Frederik Graakjær <eaafgra@students.eaaa.dk>
2021-11-02 10:30:14 +0100

    changes

---
+++ script.js
@@ -0,0 +1,42 @@
+//video
+function videoHandler(entries) {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            entry.target.play();
+        } else {
+            entry.target.pause();
+        }
+    }
+}
+
+const videoObserver = new IntersectionObserver(videoHandler)
+const video1 = document.getElementById("video1");
+videoObserver.observe(video1);
+
+//svg animation
+observer.observe(document.querySelector("svg"))
+
+//path animation
+var textPath = document.querySelector('#text-path');
+
+var textContainer = document.querySelector('#text-container');
+
+var path = document.querySelector(textPath.getAttribute('href'));
+
+var pathLength = path.getTotalLength();
+
+function updateTextPathOffset(offset) {
+    textPath.setAttribute('startOffset', offset);
+}
+
+updateTextPathOffset(pathLength);
+
+function onScroll() {
+    requestAnimationFrame(function () {
+        var rect = textContainer.getBoundingClientRect();
+        var scrollPercent = rect.y / window.innerHeight;
+        updateTextPathOffset(scrollPercent * 1 * pathLength);
+    });
+}
+
+window.addEventListener('scroll', onScroll);
